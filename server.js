60b79f7fb6c814447aa180f76f85c8797016055b
subing008 <275120420@qq.com>
2019-05-09 16:52:35 +0800

    init project

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');  
+const app = express();
+app.get('/', function (req, res) {
+ res.send('Hello,Docker!');
+});  
+app.listen(3000);
+console.log('Server is running on http://localhost:3000');
