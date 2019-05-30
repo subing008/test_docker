a562968f5a6b094ba89825f5643f84bae2a49ad9
subing008 <275120420@qq.com>
2019-05-30 14:16:43 +0800

    first

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');  
 const app = express();
 app.get('/', function (req, res) {
- res.send('Hello,Docker!');
+ res.send('第一次推送docker');
 });  
 app.listen(3000);
 console.log('Server is running on http://localhost:3000');
